08da576a9db7184ea556b3e198dd464c6b31d26b
totuworld <totuworld@gmail.com>
2022-10-05 10:46:18 +0900

    :technologist: flyio 배포 추가
    환경변수를 fly.toml에 적어야해서 해당 파일은 제거함

--- utils/get_base_url.ts
+++ utils/get_base_url.ts
@@ -2,7 +2,7 @@ export function getBaseUrl(isServer: boolean): string {
   if (isServer) {
     const protocol = process.env.PROTOCOL || 'http';
     const host = process.env.HOST || 'localhost';
-    const port = process.env.PORT || '3000';
+    const port = protocol === 'https' ? 443 : process.env.PORT || '3000';
     return `${protocol}://${host}:${port}`;
   }
   return '';
